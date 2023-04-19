c2c8f27ad19c8cfdbca023b015424eef91a38c19
Austin Jarrett <austin.jarrett.dev+github@gmail.com>
2023-04-18 23:15:01 -0400

    add links w/ dropdown

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,18 @@
+import { createPopper } from '@popperjs/core';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
   brandText = "Caitlyn's/Wyatt's Chickens";
 
-  homepageTitle = "homepage";
-  aboutTitle = "about";
-  contactTitle = "contact";
-
+  homepageTitle = 'homepage';
+  orderTitle = 'order';
+  infoTitle = 'info';
+  beefTitle = 'beef';
+  blogTitle = 'blog';
+  contactTitle = 'contact';
 }
